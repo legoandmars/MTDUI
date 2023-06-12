e5061c4897dce1addc893cac3232076165019306
legoandmars <legoandmars@gmail.com>
2023-06-12 09:09:28 -0700

    Version Bump & Add new assembly

--- MTDUI/Plugin.cs
+++ MTDUI/Plugin.cs
@@ -3,7 +3,7 @@ using MTDUI.HarmonyPatches;
 
 namespace MTDUI
 {
-    [BepInPlugin("dev.bobbie.20mtd.mtdui", "MTDUI", "1.0.3")]
+    [BepInPlugin("dev.bobbie.20mtd.mtdui", "MTDUI", "1.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake() {
